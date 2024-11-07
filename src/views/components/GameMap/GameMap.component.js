e5ebdf951fe64bda18c2436be6f6393dfad4069f
[Aquario Determinado] <aquario.determinado@gmail.com>
2024-11-07 12:28:10 +0000

    init Commit

---
+++ src/views/components/GameMap/GameMap.component.js
@@ -0,0 +1,25 @@
+import {useState} from "react";
+
+const GRID_HEIGHT = 10;
+const GRID_WIDTH = 10;
+
+function MapRow() {
+    return (
+        <div>
+            <div className="map-row">
+
+            </div>
+        </div>
+    );
+}
+
+function GameMap() {
+
+    const [grid, setGrid] = useState();
+
+
+    return (
+    <div>
+    </div>
+  );
+}
