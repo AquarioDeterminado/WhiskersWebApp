3d76e58df0bf1ff4b5b6e3e222a84cd8f6043057
[Aquario Determinado] <aquario.determinado@gmail.com>
2024-11-19 12:52:41 +0000

    Entrega Proposta;

--- src/controllers/GameManager.controller.js
+++ src/controllers/GameManager.controller.js
@@ -7,11 +7,11 @@ function GameManager({_mapObjects, _setMapObjects}) {
 
 function initMapObjects(){
     setMapObjects([
-        {position:{x: 0, y: 0}, id: 0, type: "player"},
-        {position:{x: 1, y: 1}, id: 1, type: "obstacle"},
-        {position:{x: 2, y: 2}, id: 2, type: "obstacle"},
-        {position:{x: 3, y: 3}, id: 3, type: "obstacle"},
-        {position:{x: 4, y: 4}, id: 4, type: "obstacle"}
+        {position:{x: 4, y: 2}, id: 0, type: "player"},
+        {position:{x: 3, y: 5}, id: 1, type: "obstacle"},
+        {position:{x: 4, y: 5}, id: 2, type: "obstacle"},
+        {position:{x: 6, y: 6}, id: 3, type: "obstacle"},
+        {position:{x: 2, y: 7}, id: 4, type: "obstacle"}
     ]);
 }
 
